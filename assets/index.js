7d6341b38b22cd5482ad2bfb1c0b8f9151edf40a
Delta9k8b <keaton.brewster@gmail.com>
2021-02-10 21:05:53 -0600

    added chalk for fun.

--- assets/index.js
+++ assets/index.js
@@ -2,6 +2,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const md = require("./utils/generateMarkdown.js");
+const chalk = require("chalk");
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -22,68 +23,77 @@ function inquire() {
     .prompt([
       {
         type: "input",
-        message: "What is your username?",
+        message: `${chalk.green("What is your username?")}`,
         name: "username",
       },
       {
         type: "input",
-        message: "What is your email address?",
+        message: `${chalk.green("What is your email address?")}`,
         name: "email",
       },
       {
         type: "input",
-        message:
-          "What is the title of this project? (No spaces or special characters besides -)",
+        message: `${chalk.green(
+          "What is the title of this project? (No spaces or special characters besides -)"
+        )}`,
         name: "projectTitle",
       },
       {
         type: "input",
-        message:
-          "Give a description of your application (to leave blank, type nothing)",
+        message: `${chalk.green(
+          "Give a description of your application (to leave blank, type nothing)"
+        )}`,
         name: "description",
       },
       {
         type: "list",
-        message:
-          "What method of install is recommended? (to leave blank, type nothing)",
+        message: `${chalk.green(
+          "What method of install is recommended? (to leave blank, type nothing)"
+        )}`,
         choices: ["bash", "GitHub"],
         name: "installMethod",
       },
       {
         type: "input",
-        message:
-          "What do users need to know about how to install your application? (to leave blank, type nothing)",
+        message: `${chalk.green(
+          "What do users need to know about how to install your application? (to leave blank, type nothing)"
+        )}`,
         name: "install",
       },
       {
         type: "input",
-        message:
-          "Give instructions on how to use your application. (to leave blank, type nothing)",
+        message: `${chalk.green(
+          "Give instructions on how to use your application. (to leave blank, type nothing)"
+        )}`,
         name: "usage",
       },
       {
         type: "input",
-        message:
-          "What are your contribution guidelines? (to leave blank, type nothing)",
+        message: `${chalk.green(
+          "What are your contribution guidelines? (to leave blank, type nothing)"
+        )}`,
         name: "contribution",
       },
       {
         type: "input",
-        message:
-          "What do users need to know about testing your application? (to leave blank, type nothing)",
+        message: `${chalk.green(
+          "What do users need to know about testing your application? (to leave blank, type nothing)"
+        )}`,
         name: "testI",
       },
       {
         type: "list",
-        message:
-          "Under which of the following will your application be licensed?",
+        message: `${chalk.green(
+          "Under which of the following will your application be licensed?"
+        )}`,
         choices: ["Apache 2.0", "GPL", "Eclipse", "MIT", "Not licensed"],
         name: "license",
       },
       {
         type: "input",
-        message:
-          "What technologies are utilized in your application? (to leave blank, type nothing)",
+        message: `${chalk.green(
+          "What technologies are utilized in your application? (to leave blank, type nothing)"
+        )}`,
         name: "technologies",
       },
     ])
